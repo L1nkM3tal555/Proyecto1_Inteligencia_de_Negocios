175c609c04b47ba9b10d7b3b6110f8cc09792e86
Jesús Alejandro Dávila <69691593+L1nkM3tal555@users.noreply.github.com>
2023-10-29 18:56:47 -0500

    Back end v1

---
+++ Back-end/api.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from joblib import load
+
+# Cargar el modelo
+pipeline = load('Modelo predictivo\model.joblib')
+
+# FastAPI
+app = FastAPI()
+
+# Define el esquema para los datos de entrada
+class InputData(BaseModel):
+    features: list
+
+# Define la ruta para realizar predicciones
+@app.post('/predict/')
+def predict(data: InputData):
+    try:
+        # Realiza la predicción utilizando el pipeline cargado
+        prediction = pipeline.predict([data.features])[0]
+        return {'prediction': prediction}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# Ejecuta la aplicación FastAPI
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
